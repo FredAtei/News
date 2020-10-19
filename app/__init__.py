17c5a1243451f6d2de6f8d5f30a4c9f5f54323fd
Fred <akamaatei@gmail.com>
2020-10-19 10:56:12 +0300

    deployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
+from .config import Config
 
-# Initializing application
-app = Flask(__name__)
-
+app = Flask(__name__,instance_relative_config = True)
+app.config.from_object(Config)
 from app import views
---
+++ app/config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config:
+    API_KEY=os.environ.get("API_KEY")
+    NEWS_API_BASE_URL= 'https://newsapi.org/v2/{}?apiKey={}'
